95ba66eae9e8804fcb04265461b31461b853e5a5
SharpShooter <b.ujazdowski@gmail.com>
2018-06-05 11:42:16 +0200

    Wytlumaczenei zadania 1

--- Zadanie_1/src/Zadanie_1.cpp
+++ Zadanie_1/src/Zadanie_1.cpp
@@ -12,11 +12,23 @@ void * thread(void * params)
 	delete signalParams;
 
 	sigset_t sigset;
+	/**
+	 * Informacja o sygnale
+	 */
 	siginfo_t siginfo;
 
+	/**
+	 * sigfillset - inicjalizuje zbiór aby zawierał wszystkie sygnały
+	 */
 	sigfillset(&sigset);
+	/**
+	 * sigdelset - Usuwa ze zbioru jeden sygnal
+	 */
 	sigdelset(&sigset, numOfSignal);
 
+	/**
+	 * pthread_sigmask - który wątek ma być wywoływany dla danego sygnału
+	 */
 	pthread_sigmask(SIG_SETMASK, &sigset, NULL);
 
 	sigemptyset(&sigset);
@@ -24,6 +36,9 @@ void * thread(void * params)
 
 	while (true)
 	{
+		/**
+		 * Czeka na odpowiedni sygnał i zwraca informacje
+		 */
 		int currentSignal = sigwaitinfo(&sigset, &siginfo);
 		std::cout << "Signal number: " << currentSignal << " data: " <<  siginfo.si_value.sival_ptr << std::endl;
 	}
@@ -39,15 +54,28 @@ void theEnd( const std::string & info)
 
 int main()
 {
+	/**
+	 * SIGRTMIN - SIGRTMAX - Ilość sygnałów czasu rzeczywistego (41 - 56)
+	 */
 	const int size = SIGRTMAX - SIGRTMIN + 1;
 	pthread_t threads[size];
+
+	/**
+	 * 2 x Tablica long int
+	 */
 	sigset_t sigset;
 
+	/**
+	 * Czyszczenie sigset - nie zawiera sygnałów
+	 */
 	sigemptyset(&sigset);
 
 	for (int id = 0; id < size; id++)
 	{
 		int signalNumber = id + SIGRTMIN;
+		/**
+		 * Dodawanie sygnału do zbioru
+		 */
 		sigaddset(&sigset, signalNumber);
 		SignalParam * params = new SignalParam();
 		params->signal = signalNumber;
@@ -59,6 +87,11 @@ int main()
 		}
 	}
 
+
+	/**
+	 * Modyfikuje maske sygnału
+	 * SIG_BLOCK - Tworzy unie z maski
+	 */
 	pthread_sigmask(SIG_BLOCK, &sigset, NULL);
 
 	for ( int id = 0; id < size; id++) {
